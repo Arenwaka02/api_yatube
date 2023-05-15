0c6cb13dc81717d74c610cac957f6c5baf44c7d3
Airat <airatshaiahmetov@yandex.ru>
2023-05-15 22:27:28 +0300

    aaa

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import get_object_or_404
-
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from .permissions import IsAuthorOrReadOnly
 from .serializers import CommentSerializer, GroupSerializer, PostSerializer
-
 from posts.models import Group, Post
 
 
